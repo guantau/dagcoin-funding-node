00351ca586907f13ed4c85dbad43896afd128573
Yary Ribero <yary.ribero@ignite.ee>
2017-09-25 16:06:57 +0300

    The funding-node now:
     - shares addresses
     - funds them
     - accepts transactions if there is an output for them

--- submodules/fundingExchangeProviderService.js
+++ submodules/fundingExchangeProviderService.js
@@ -1,5 +1,7 @@
-function FundingExchangeProvider (pairingString) {
+function FundingExchangeProvider (pairingString, xPrivKey) {
     this.conf = require('byteballcore/conf.js');
+    this.eventBus = require('byteballcore/event_bus');
+    this.db = require('byteballcore/db');
 
     this.exchangeFee = this.conf.exchangeFee;
     this.totalBytes = this.conf.totalBytes;
@@ -8,9 +10,20 @@ function FundingExchangeProvider (pairingString) {
 
     this.active = false;
     this.pairingString = pairingString;
+    this.xPrivKey = xPrivKey;
 
     const DiscoveryService = require('./discoveryService');
     this.discoveryService = new DiscoveryService();
+
+    console.log(`pairingString: ${this.pairingString}`);
+    console.log(`xPrivKey: ${this.xPrivKey}`);
+
+    this.initDagcoinDestination().then((myAddress) => {
+        console.log(`MY ADDRESS INITIALISED TO ${myAddress}`);
+    }).catch((err) => {
+        console.log(`ERROR: ${err}`);
+        process.exit();
+    });
 }
 
 FundingExchangeProvider.prototype.activate = function () {
@@ -20,8 +33,7 @@ FundingExchangeProvider.prototype.activate = function () {
 
     const self = this;
 
-    return this.discoveryService.startingTheBusiness(this.pairingString)
-    .then((response) => {
+    return this.discoveryService.startingTheBusiness(this.pairingString).then((response) => {
         if (response) {
             self.active = true;
             console.log(`RECEIVED A RESPONSE FOR ${self.discoveryService.messages.startingTheBusiness}: ${JSON.stringify(response)}`);
@@ -31,6 +43,12 @@ FundingExchangeProvider.prototype.activate = function () {
             console.log('NO REPLY FROM THE DISCOVERY SERVICE. CAN\'T BE SURE WHETHER IT ACCEPTED MY PROPOSAL.');
             return Promise.reject();
         }
+    }).then(() => {
+        console.log("ADDING REACTION TO dagcoin.request.share-funded-address");
+        self.eventBus.on('dagcoin.request.share-funded-address', (message, deviceAddress) => {
+            console.log(`REQUEST TO SHARE AN ADDRESS FROM ${deviceAddress}: ${JSON.stringify(message)}`);
+            self.shareFundedAddress(deviceAddress, message);
+        });
     });
 }
 
@@ -81,5 +99,271 @@ FundingExchangeProvider.prototype.keepAlive = function () {
         }, 10 * 60 * 1000);
     });
 }
+
+FundingExchangeProvider.prototype.initDagcoinDestination = function () {
+    const self = this;
+
+    if (self.dagcoinDestination) {
+        return Promise.resolve(self.dagcoinDestination);
+    }
+
+    return new Promise((resolve, reject) => {
+        self.db.query("SELECT wallet FROM wallets", function(rows){
+            if (rows.length === 0) {
+                reject('NO WALLETS FOUND');
+            } else if (rows.length > 1) {
+                reject(`MORE THAN 1 WALLET FOUND: ${rows.length} ARE CURRENTLY IN THE DATABASE`);
+            } else {
+                console.log('WALLET FOUND');
+                resolve(rows[0].wallet);
+            }
+        });
+    }).then((walletId) => {
+        return new Promise((resolve, reject) => {
+            self.walletDefinedByKeys.readAddresses(walletId, {}, (rows) => {
+                if (rows.length == 0) {
+                    reject('NO ADDRESSES AVAILABLE');
+                } else {
+                    self.dagcoinDestination = rows[0].address;
+                    resolve(self.dagcoinDestination);
+                }
+            });
+        });
+    });
+}
+
+FundingExchangeProvider.prototype.shareFundedAddress = function (remoteDeviceAddress, message) {
+    const remoteAddress = message.address;
+
+    if(!remoteAddress) {
+        return Promise.reject(`NO ADDRESS FOUND IN THE MESSAGE: ${JSON.stringify(message)}`);
+    }
+
+    console.log(`REQUEST FROM ${remoteDeviceAddress}:${remoteAddress} ${JSON.stringify(message)}`);
+
+    this.initComponents();
+
+    const self = this;
+    const myDeviceAddress = this.device.getMyDeviceAddress();
+
+    console.log('STARTING THE ADDRESS GENERATION PROCESS');
+
+    return this.initDagcoinDestination().then((myAddress) => {
+        console.log(`MY ADDRESS: ${myAddress}`);
+
+        const addressDefinitionTemplate = JSON.parse(`["and", [["address", "$address@${myDeviceAddress}"], ["address", "$address@${remoteDeviceAddress}"]]]`);
+        const sharedAddress = this.objectHash.getChash160(addressDefinitionTemplate);
+
+        return new Promise(function (resolve, reject) {
+            self.db.query(
+                'SELECT definition_template_chash FROM pending_shared_addresses WHERE definition_template_chash = ?',
+                [sharedAddress],
+                function (rows) {
+                    console.log(`FOUND ${rows.length} WITH definition_template_chash ${sharedAddress}`);
+                    if(rows.length > 0) {
+                        reject("ALREADY ON PROCESSING");
+                    }
+
+                    console.log(`ADDRESS DEFINITION TEMPLATE: ${JSON.stringify(addressDefinitionTemplate)}`);
+
+                    self.walletDefinedByAddress.createNewSharedAddressByTemplate(addressDefinitionTemplate, myAddress, {"r": myDeviceAddress });
+
+                    resolve(sharedAddress);
+                }
+            );
+        });
+    }).then((sharedAddress) => {
+        console.log(`SHARED ADDRESS: ${sharedAddress}`);
+
+
+        const response = {
+            protocol: 'dagcoin',
+            title: 'response.share-funded-address',
+            byteOrigin: sharedAddress,
+            dagcoinDestination: self.dagcoinDestination
+        };
+
+        if (message.id !== null) {
+            response.id = message.id;
+        }
+
+        return new Promise((resolve, reject) => {
+            self.device.sendMessageToDevice(
+                remoteDeviceAddress,
+                'text',
+                JSON.stringify(response),
+                {
+                    ifOk() {
+                        resolve(sharedAddress);
+                    },
+                    ifError(error) {
+                        reject(error);
+                    }
+                }
+            );
+        });
+    });
+}
+
+FundingExchangeProvider.prototype.initComponents = function () {
+    if (!this.mutex) {
+        this.mutex = require('byteballcore/mutex.js');
+    }
+
+    if (!this.device) {
+        this.device = require('byteballcore/device.js');
+    }
+
+    if (!this.db) {
+        this.db = require('byteballcore/db');
+    }
+
+    if (!this.objectHash) {
+        this.objectHash = require('byteballcore/object_hash');
+    }
+
+    if (!this.walletDefinedByKeys) {
+        this.walletDefinedByKeys = require('byteballcore/wallet_defined_by_keys');
+    }
+
+    if (!this.walletDefinedByAddress) {
+        this.walletDefinedByAddress = require('byteballcore/wallet_defined_by_addresses');
+    }
+
+    if (!this.async) {
+        this.async = require('async');
+    }
+
+    if (!this.constants) {
+        this.constants = require('byteballcore/constants.js');
+    }
+
+    if (!this.ecdsaSig) {
+        this.ecdsaSig = require('byteballcore/signature.js');
+    }
+}
+
+FundingExchangeProvider.prototype.handleSharedPaymentRequest = function () {
+    var assocChoicesByUnit = {};
+
+    this.initComponents();
+
+    const self = this;
+
+    self.eventBus.on("signing_request", function(objAddress, top_address, objUnit, assocPrivatePayloads, from_address, signing_path){
+
+        function createAndSendSignature(){
+            var coin = "0";
+            var path = "m/44'/" + coin + "'/" + objAddress.account + "'/"+objAddress.is_change+"/"+objAddress.address_index;
+            console.log("path "+path);
+
+            var privateKey = self.xPrivKey.derive(path).privateKey;
+            console.log("priv key:", privateKey);
+            //var privKeyBuf = privateKey.toBuffer();
+            var privKeyBuf = privateKey.bn.toBuffer({size:32}); // https://github.com/bitpay/bitcore-lib/issues/47
+            console.log("priv key buf:", privKeyBuf);
+            var buf_to_sign = self.objectHash.getUnitHashToSign(objUnit);
+            var signature = self.ecdsaSig.sign(buf_to_sign, privKeyBuf);
+            bbWallet.sendSignature(from_address, buf_to_sign.toString("base64"), signature, signing_path, top_address);
+            console.log("sent signature "+signature);
+        }
+
+        function refuseSignature(){
+            var buf_to_sign = self.objectHash.getUnitHashToSign(objUnit);
+            bbWallet.sendSignature(from_address, buf_to_sign.toString("base64"), "[refused]", signing_path, top_address);
+            console.log("refused signature");
+        }
+
+        var bbWallet = require('byteballcore/wallet.js');
+        var unit = objUnit.unit;
+        self.mutex.lock(["signing_request-"+unit], function(unlock){
+
+            // apply the previously obtained decision.
+            // Unless the priv key is encrypted in which case the password request would have appeared from nowhere
+            if (assocChoicesByUnit[unit]){
+                if (assocChoicesByUnit[unit] === "approve")
+                    createAndSendSignature();
+                else if (assocChoicesByUnit[unit] === "refuse")
+                    refuseSignature();
+                return unlock();
+            }
+
+            self.walletDefinedByKeys.readChangeAddresses(objAddress.wallet, function(arrChangeAddressInfos){
+                var arrAuthorAddresses = objUnit.authors.map(function(author){ return author.address; });
+                var arrChangeAddresses = arrChangeAddressInfos.map(function(info){ return info.address; });
+                arrChangeAddresses = arrChangeAddresses.concat(arrAuthorAddresses);
+                arrChangeAddresses.push(top_address);
+                var arrPaymentMessages = objUnit.messages.filter(function(objMessage){ return (objMessage.app === "payment"); });
+                if (arrPaymentMessages.length === 0)
+                    throw Error("no payment message found");
+                var assocAmountByAssetAndAddress = {};
+                // exclude outputs paying to my change addresses
+                self.async.eachSeries(
+                    arrPaymentMessages,
+                    function(objMessage, cb){
+                        var payload = objMessage.payload;
+                        if (!payload)
+                            payload = assocPrivatePayloads[objMessage.payload_hash];
+                        if (!payload)
+                            throw Error("no inline payload and no private payload either, message="+JSON.stringify(objMessage));
+                        var asset = payload.asset || "base";
+                        if (!payload.outputs)
+                            throw Error("no outputs");
+                        if (!assocAmountByAssetAndAddress[asset])
+                            assocAmountByAssetAndAddress[asset] = {};
+                        payload.outputs.forEach(function(output){
+                            if (arrChangeAddresses.indexOf(output.address) === -1){
+                                if (!assocAmountByAssetAndAddress[asset][output.address])
+                                    assocAmountByAssetAndAddress[asset][output.address] = 0;
+                                assocAmountByAssetAndAddress[asset][output.address] += output.amount;
+                            }
+                        });
+                        cb();
+                    },
+                    function(){
+                        var unitName = "bytes";//config.unitName;
+                        var bbUnitName = 'blackbytes';//config.bbUnitName;
+
+                        var arrDestinations = [];
+                        for (var asset in assocAmountByAssetAndAddress){
+
+                            var currency = "of asset "+asset;
+                            var assetName = asset;
+                            if(asset === 'base'){
+                                currency = unitName;
+                                assetName = 'base';
+                            }else if(asset === self.constants.BLACKBYTES_ASSET){
+                                currency = bbUnitName;
+                                assetName = 'blackbytes';
+                            }
+                            for (var address in assocAmountByAssetAndAddress[asset])
+                                arrDestinations.push(assocAmountByAssetAndAddress[asset][address] + assetName + " " + currency + " to " + address);
+                        }
+                        var dest = (arrDestinations.length > 0) ? arrDestinations.join(", ") : "to myself";
+                        // var question = 'Sign transaction spending '+dest+' from wallet '+wallet_id+'?';
+                        // console.log(question);
+
+                        console.log(JSON.stringify(assocAmountByAssetAndAddress));
+                        console.log(self.dagcoinDestination);
+                        console.log(JSON.stringify(assocAmountByAssetAndAddress[self.conf.dagcoinAsset][self.dagcoinDestination]));
+
+                        if (assocAmountByAssetAndAddress[self.conf.dagcoinAsset][self.dagcoinDestination] >= 1000) {
+                            //APPROVED if there is an output to the base address of some dagcoins
+                            createAndSendSignature();
+                            assocChoicesByUnit[unit] = "approve";
+                            unlock();
+                        } else { //NOT APPROVED
+                            console.log("===== NO CLICKED");
+                            refuseSignature();
+                            assocChoicesByUnit[unit] = "refuse";
+                            unlock();
+                        }
+                    }
+                ); // eachSeries
+            });
+        });
+    });
+}
+
 
 module.exports = FundingExchangeProvider;
